febae366c14575cce16ef8ad33098b89ae454b7c
sergey.lezhenko <s.lezhenko.developer@gmail.com>
2016-07-06 11:37:59 +0300

    Edit styles for left menu panel

--- views/layouts/admin.php
+++ views/layouts/admin.php
@@ -97,26 +97,24 @@ Modal::end();
 		<span>Admin Area</span>
 		<ul class="item_list">
 			<li class="item_1"><a href="#">Dashboard</a></li>
-			<li class="item_2"><a href="#">Availability Calendar</a></li>
-			<li class="item_3"><a href="#">Schedule</a></li>
-			<li class="item_4"><a href="#">Customers</a></li>
-			<li class="item_5"><a href="#">Inquiries</a></li>
-			<li class="item_6"><a href="#">To do</a></li>
-			<li class="item_7"><a href="#">Pending Answers</a></li>
-			<li class="item_8"><a href="#">Messages</a></li>
-			<li class="item_9"><a href="#">Quotes</a></li>
-			<li class="item_10"><a href="#">Invoices</a></li>
-			<li class="item_11<?php echo (Yii::$app->controller->id == 'admin-venue') ? ' active' : ''; ?>"><a href="<?php echo Url::to(['admin-venue/index']); ?>">Venues</a></li>
-			<li class="item_12"><a href="#">Vendors</a></li>
-			<li class="item_13"><a href="#">Locations</a></li>
-			<li class="item_14"><a href="#">Pricing</a></li>
-			<li class="item_15 <?php echo (Yii::$app->controller->id == 'admin-knowledgebases') ? 'active' : ''; ?>"><a href="<?php echo Url::to(['admin-knowledgebases/index']); ?>" id="it_15">Knowledge Base</a></li>
-			<li class="item_16"><a href="#">Reports</a></li>
-			<li class="item_17"><a href="#">Webs</a></li>
-			<li class="item_18"><a href="#">Settings</a></li>
-			<li class="item_19"><a href="#">User Manager</a></li>
-			<li class="item_20"><a href="#">Companies</a></li>
-			<li class="item_21 <?php echo (Yii::$app->controller->id == 'admin-mastertable') ? 'active' : ''; ?>"><a href="<?php echo Url::to(['admin-mastertable/index']); ?>">Mastertable</a></li>
+			<li class="item_2"><a href="#">Schedule</a></li>
+			<li class="item_3"><a href="#">Customers</a></li>
+			<li class="item_4"><a href="#">Inquiries</a></li>
+			<li class="item_5"><a href="#">To do</a></li>
+			<li class="item_6"><a href="#">Pending Answers</a></li>
+			<li class="item_7"><a href="#">Messages</a></li>
+			<li class="item_8"><a href="#">Quotes</a></li>
+			<li class="item_9"><a href="#">Invoices</a></li>
+			<li class="item_10<?php echo (Yii::$app->controller->id == 'admin-venue') ? ' active' : ''; ?>"><a href="<?php echo Url::to(['admin-venue/index']); ?>">Venues</a></li>
+			<li class="item_11"><a href="#">Vendors</a></li>
+			<li class="item_12"><a href="#">Agencies</a></li>
+			<li class="item_13 <?php echo (Yii::$app->controller->id == 'admin-knowledgebases') ? 'active' : ''; ?>"><a href="<?php echo Url::to(['admin-knowledgebases/index']); ?>">Knowledge Base</a></li>
+			<li class="item_14"><a href="#">Reports</a></li>
+			<li class="item_15"><a href="#">Our Profile</a></li>
+			<li class="item_16"><a href="#">Our Packages</a></li>
+			<li class="item_17"><a href="#">Our Items</a></li>
+			<li class="item_18"><a href="#">User Manager</a></li>
+			<li class="item_19 <?php echo (Yii::$app->controller->id == 'admin-mastertable') ? 'active' : ''; ?>"><a href="<?php echo Url::to(['admin-mastertable/index']); ?>">Mastertable</a></li>
 		</ul>
 	</div>
 	<header>
