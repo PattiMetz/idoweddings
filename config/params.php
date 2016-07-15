d19dcd01bd5e9a746835a8258df0286157b07525
Irina <ipodzemelnaya@gmail.com>
2016-07-15 16:37:28 +0600

    params update

--- config/params.php
+++ config/params.php
@@ -2,5 +2,7 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'shortcodes' =>['{%vendor%}'=>'Vendor Name', '{%agency%}' => 'Agency Name', '{%groom%}'=>'Groom Name', '{%bride%}'=>'Bride Name', '{%venue%}'=>'Venue Name', '{%main_company_name%}' => 'Main Company Name']
+    'shortcodes' => ['{%vendor%}' => 'Vendor Name', '{%agency%}' => 'Agency Name', '{%groom%}' => 'Groom Name', '{%bride%}' => 'Bride Name', '{%venue%}' => 'Venue Name', '{%main_company_name%}' => 'Main Company Name'],
+    'slide' => ['width' => 1366, 'height' => 530],
+    'fonts'  => ['Times New Roman'=>'Times New Roman', 'Arial'=>'Arial']
 ];
