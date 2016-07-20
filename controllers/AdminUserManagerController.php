fa63a07483c2ed54abb884ebbdfb85ae190124df
Vlad <vlads.developer@gmail.com>
2016-07-20 16:37:31 +0300

    User manager (start commit)

--- controllers/AdminUserManagerController.php
+++ controllers/AdminUserManagerController.php
@@ -2,14 +2,181 @@
 
 namespace app\controllers;
 
+use Yii;
 use yii\web\Controller;
+use yii\web\Response;
+use app\models\search\UserSearch;
+use app\models\Role;
+use app\models\Privilege;
+use yii\db\Query;
+use yii\helpers\ArrayHelper;
+use yii\data\ArrayDataProvider;
+use yii\filters\AccessControl;
+use app\components\AccessRule;
 
-class AdminUserManagerController extends Controller
-{
-    public $layout = 'admin';
+class AdminUserManagerController extends Controller {
 
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
+	public $layout = 'admin';
+
+	public function beforeAction($action) {
+
+		$this->view->params['section_title'] = 'User Manager';
+
+		return parent::beforeAction($action);
+
+	}
+
+	public function behaviors() {
+
+		return [
+			'access' => [
+				'class' => AccessControl::className(),
+//				'ruleConfig' => [
+//					'class' => AccessRule::className(),
+//				],
+				'rules' => [
+					[
+						'allow' => true,
+//						'roles' => ['usermanager'],
+					],
+				],
+			],
+		];
+
+	}
+
+	public function actionIndex() {
+
+		$this->view->title = 'Users';
+
+		$searchModel = new UserSearch();
+
+		$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+		return $this->render('index', [
+			'searchModel' => $searchModel,
+			'dataProvider' => $dataProvider
+		]);
+
+	}
+
+	public function actionRoles() {
+
+#var_dump(Yii::$app->user->identity->privilegesNames);
+
+#var_dump(Yii::$app->user->identity->hasPrivilegeByName('usermanager'));
+
+#var_dump(Yii::$app->user->identity->getPrivilegesNames());
+
+		$this->view->title = 'Roles';
+
+		$companyTypes = ArrayHelper::map((new Query)->from('company_type')->all(), 'id', 'name');
+
+		$dataProviders = [];
+
+		foreach (array_keys($companyTypes) as $companyTypeId) {
+
+			$dataProviders[$companyTypeId] = new ArrayDataProvider([
+				'allModels' => []
+			]);
+
+		}
+
+		$templateRoles = Role::find()->where([
+			'company_id' => 0,
+			'company_type_id' => array_keys($companyTypes)
+		])->all();
+
+		foreach ($templateRoles as $role) {
+
+			$dataProviders[$role->company_type_id]->allModels[] = $role;
+
+		}
+
+		return $this->render('roles', compact(
+			'companyTypes',
+			'dataProviders'
+		));
+
+	}
+
+	function actionUpdateRole($id) {
+
+		$this->view->title = 'Update Role';
+
+		if ($id) {
+
+			$model = Role::findOne($id);
+
+			if (!$model) {           
+
+				throw new \yii\web\NotFoundHttpException('Role not found');
+
+			}
+
+			$model->privilege_ids = $model->getPrivileges()->select('id')->column();
+
+		} else {
+
+			$model = new Role;
+
+			$model->privilege_ids = [];
+
+		}
+
+		$model->privilegesTreeInfo = (new Privilege)->getTreeInfo();
+
+		if (Yii::$app->request->isPost) {
+		
+			$post = Yii::$app->request->post();
+			
+			if (empty($post['privilege_ids'])) {
+
+				$post['privilege_ids'] = [];
+
+			}
+
+			$model->load($post);
+
+			if ($model->save()) {
+
+				// Unlink all privileges
+				$model->unlinkAll('privileges', true);
+
+				foreach ($model->privilege_ids as $privilege_id) {
+
+					if (isset($model->privilegesTreeInfo['flat_tree'][$privilege_id])) {
+
+						$privilege = $model->privilegesTreeInfo['flat_tree'][$privilege_id];
+
+						// Link privilege
+						$model->link('privileges', $privilege);
+
+					}
+
+				}
+
+			} else {
+
+				$errors = $model->getErrors();
+
+			}
+
+			$pjax_reload = '#main';
+
+			Yii::$app->response->format = Response::FORMAT_JSON;
+
+			return compact(
+				'errors',
+				'pjax_reload'
+			);
+
+		}
+
+		return $this->renderAjax('update-role', compact(
+			'model'
+		));
+
+	}
+
 }
--- views/admin-user-manager/roles.php
+++ views/admin-user-manager/roles.php
@@ -6,11 +6,15 @@ use yii\grid\GridView;
 
 <?php echo $this->render('_tabs'); ?>
 
+
+<?php foreach ($dataProviders as $companyTypeId => $dataProvider): ?>
+
+	<h4>Roles and privileges for <?php echo Html::encode($companyTypes[$companyTypeId]); ?></h4>
 	<div class="table-responsive">
 	<?php
 		echo GridView::widget([
 			'dataProvider' => $dataProvider,
-			'layout' => "{items}\n{summary}\n{pager}",
+			'layout' => "{items}",
 			'tableOptions' => [
 				'class' => 'table table-bordered'
 			],
@@ -19,21 +23,16 @@ use yii\grid\GridView;
 				[  
 					'class' => 'yii\grid\ActionColumn',
 					'header' => 'Actions',
-					'template' => '{update} {view} {delete}',
+					'template' => '{update} {delete}',
 					'urlCreator' => function ($action, $model, $key, $index) {
-/*						$type = ($model->is_category) ? 'categories' : 'articles';
 						switch ($action) {
 							case 'update':
-								$url = Url::to(["admin-knowledgebases/{$type}-update", 'id' => $model->id]);
-								break;
-							case 'view':
-								$url = Url::to(["admin-knowledgebases/{$type}-view", 'id' => $model->id]);
+								$url = Url::to(['admin-user-manager/update-role', 'id' => $model->id]);
 								break;
 							case 'delete':
-								$url = Url::to(["admin-knowledgebases/{$type}-delete", 'id' => $model->id]);
+								$url = Url::to(['admin-user-manager/role-delete', 'id' => $model->id]);
 								break;
-						}*/
-						$url = '';
+						}
 						return $url;
 					},
 					'buttons' => [
@@ -41,13 +40,6 @@ use yii\grid\GridView;
 							return Html::button('<i class="glyphicon glyphicon-pencil"></i> Edit', [
 								'value' => $url,
 								'class' => 'btn btn-primary modal-ajax',
-							]);
-						},
-						'view' => function($url, $model) {
-							return Html::button('<i class="glyphicon glyphicon-mykey"></i>', [
-								'value' => $url,
-								'class' => 'btn btn-danger modal-ajax',
-								'title' => 'Permissins',
 							]);
 						},
 						'delete' => function($url, $model) {
@@ -62,3 +54,5 @@ use yii\grid\GridView;
 		]);
 	?>
 	</div>
+
+<?php endforeach; ?>
