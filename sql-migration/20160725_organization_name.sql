6a40808690b7941450473a87f36e737ba76da2b1
Vlad <vlads.developer@gmail.com>
2016-08-05 20:03:09 +0300

    Fix

--- sql-migration/20160725_organization_name.sql
+++ sql-migration/20160725_organization_name.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `organization` ADD `name` VARCHAR(40) NOT NULL;
-UPDATE `idoweddings`.`organization` SET `name` = 'WOMI' WHERE `organization`.`id` = 1;
+UPDATE `organization` SET `name` = 'WOMI' WHERE `id` = 1;
