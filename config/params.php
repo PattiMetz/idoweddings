3deb3354b6a3b52f9a6e2e8609b978681d50dbf2
Irina <ipodzemelnaya@gmail.com>
2016-10-19 17:01:51 +0600

    merge

--- config/params.php
+++ config/params.php
@@ -4,5 +4,5 @@ return [
     'adminEmail' => 'admin@example.com',
     'shortcodes' => ['{%vendor%}' => 'Vendor Name', '{%agency%}' => 'Agency Name', '{%groom%}' => 'Groom Name', '{%bride%}' => 'Bride Name', '{%venue%}' => 'Venue Name', '{%main_company_name%}' => 'Main Company Name'],
     'slide' => ['width' => 1366, 'height' => 530],
-    'fonts'  => ['Times New Roman'=>'Times New Roman', 'Arial'=>'Arial']
+    'fonts'  => ['Times New Roman'=>'Times New Roman', 'Arial'=>'Arial','Conv_EuphoriaScript-Regular'=>'Conv_EuphoriaScript-Regular','Conv_proximanova-regular'=>'Conv_proximanova-regular']
 ];
